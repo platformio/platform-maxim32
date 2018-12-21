a02a9a4867bdfcec80ca7afa519c8dd28e53d278
Ivan Kravets <me@ikravets.com>
2018-12-21 17:09:22 +0200

    Fix warning about "Unknown upload protocol custom"

--- builder/main.py
+++ builder/main.py
@@ -152,7 +152,7 @@ elif upload_protocol.startswith("jlink"):
     upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
 
 # custom upload tool
-elif "UPLOADCMD" in env:
+elif upload_protocol == "custom":
     upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
 
 else:
